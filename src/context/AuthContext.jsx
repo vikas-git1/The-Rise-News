5d14fa36eb38daf984e21b36f40bd10603109011
vikas-git1 <vikasmewara.dev@gmail.com>
2025-07-07 18:58:01 +0530

    Add Auth Context

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { createContext, useContext, useEffect, useState } from "react";
+import { auth, db } from "../auth/firebase";
+import { getDoc, doc } from "firebase/firestore";
+import { onAuthStateChanged } from "firebase/auth";
+export const AuthContext = createContext();
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [userProfile, setUserProfile] = useState({});
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, async (userDetails) => {
+      setUser(userDetails);
+      if (userDetails) {
+        const docRef = doc(db, "users", userDetails.uid);
+        const docSnap = await getDoc(docRef);
+        if (docSnap.exists()) {
+          setUserProfile(docSnap.data());
+        } else {
+          setUserProfile({});
+        }
+      } else {
+        setUser(null);
+        setUserProfile({});
+      }
+    });
 
-const AuthContext = () => {
-  return <div></div>;
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user, userProfile }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
-
-export default AuthContext;
+export const useAuth = () => useContext(AuthContext);
